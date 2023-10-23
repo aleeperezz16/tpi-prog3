45e119f5bb0ed0e26f312773a0895896951261ad
Alejo <alejoperezherrero@gmail.com>
2023-10-23 14:11:01 -0300

    Modulo poder modificar cliente

--- Dao/DaoClientes.cs
+++ Dao/DaoClientes.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using System.Data.SqlClient;
+using Entidades;
 
 namespace Dao
 {
@@ -11,6 +13,13 @@ namespace Dao
     {
         private AccesoDatos _datos = new AccesoDatos("TPIntegradorGrupo6");
         public DaoClientes() { }
+
+        public int ModificarCliente(Cliente cli)
+        {
+            SqlCommand cmd = new SqlCommand();
+            ArmarParametrosModificar(ref cmd, cli);
+            return _datos.EjecutarProcedimientoAlmacenado(ref cmd, "sp_ModificarCliente");
+        }
 
         public DataTable ObtenerClientes(int id)
         {
@@ -20,6 +29,19 @@ namespace Dao
                 consulta += $" WHERE Dni = {id}";
 
             return _datos.ObtenerTabla("Clientes", consulta);
+        }
+
+        private void ArmarParametrosModificar(ref SqlCommand cmd, Cliente cli)
+        {
+            cmd.Parameters.AddWithValue("@DNI", cli.Dni);
+            cmd.Parameters.AddWithValue("@APELLIDO", cli.Apellido);
+            cmd.Parameters.AddWithValue("@ALIAS", cli.Usuario.Alias);
+            cmd.Parameters.AddWithValue("@CONTRASENIA", cli.Usuario.Contrasenia);
+            cmd.Parameters.AddWithValue("@TELEFONO", cli.Telefono);
+            cmd.Parameters.AddWithValue("@EMAIL", cli.Email);
+            cmd.Parameters.AddWithValue("@DIRECCION", cli.Direccion);
+            cmd.Parameters.AddWithValue("@CODCIUDAD", cli.Ciudad.Codigo);
+            cmd.Parameters.AddWithValue("@ESTADO", cli.Estado);
         }
     }
 }
