c6d7affa9cad3a3809374465d6e4bf392c9c35b0
CoronelJJ <jjcoronel104@gmail.com>
2023-10-30 11:56:58 -0300

    dao ciudad y provincia

---
+++ Dao/DaoProvincia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Dao
+{
+    public class DaoProvincia
+    {
+        private AccesoDatos _datos = new AccesoDatos("TPIntegradorGrupo6");
+
+        public DaoProvincia() { }
+
+        public DataTable ObtenerProvincias(int id)
+        {
+            string consulta = "SELECT * FROM PROVINCIA";
+
+            if (id > 0)
+                consulta += $" WHERE IDProvincia= {id}";
+
+            return _datos.ObtenerTabla("Provincias", consulta);
+
+        }
+    }
+}
